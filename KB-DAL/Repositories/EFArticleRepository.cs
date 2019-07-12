d3e2358d39be4828387c88cf19c8a7151cbde3dd
a.snigirev <dmbsseller@gmail.com>
2019-07-12 20:31:24 +0800

    configure DAL, create interfaces and EFRepository classes

---
+++ KB-DAL/Repositories/EFArticleRepository.cs
@@ -0,0 +1,65 @@
+﻿using KB_DAL.Interfaces;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace KB_DAL.Repositories
+{
+    /// <summary>
+    /// Article repository with CRUD operation and save domain articles in Database
+    /// </summary>
+    public class EFArticleRepository : IArticleRepository
+    {
+        private KBEntities context = new KBEntities();
+
+        public IQueryable<Article> Articles
+        {
+            get
+            {
+                return context.Articles;
+            }
+        }
+        /// <summary>
+        /// Create article with save in DB
+        /// </summary>
+        /// <param name="article"></param>
+        public void CreateArticle (Article article)
+        {
+            if (article == null)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                context.Articles.Add(article);
+            }
+            context.SaveChanges();
+        }
+        /// <summary>
+        /// Edit article with save in DB
+        /// </summary>
+        /// <param name="article"></param>
+        public void EditArticle (Article article)
+        {
+            if (article == null)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                context.Entry(article).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// Delete article with save in DB
+        /// </summary>
+        /// <param name="articleId"></param>
+        public void DeleteArticle (int articleId)
+        {
+            var article = context.Articles.FirstOrDefault(x => x.Id == articleId);
+            context.Articles.Remove(article ?? throw new InvalidOperationException());
+            context.SaveChanges();
+        }
+    }
+}
---
+++ KB-DAL/Repositories/EFCategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using KB_DAL.Interfaces;
+using System;
+using System.Linq;
+
+namespace KB_DAL.Repositories
+{
+    /// <summary>
+    /// Category repository with CRUD operation and save domain categories in Database
+    /// </summary>
+    public class EFCategoryRepository : ICategoryRepository
+    {
+        private KBEntities context = new KBEntities();
+
+        public IQueryable<Category> Categories
+        {
+            get
+            {
+                return context.Categories;
+            }
+        }
+        /// <summary>
+        /// Create Category with save in DB
+        /// </summary>
+        /// <param name="category"></param>
+        public void CreateCategory(Category category)
+        {
+            if (category == null)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                context.Categories.Add(category);
+            }
+            context.SaveChanges();
+        }
+        /// <summary>
+        /// Delete Category with save in DB
+        /// </summary>
+        /// <param name="CategoryId"></param>
+        public void DeleteCategory (int CategoryId)
+        {
+            Category dbEntry = context.Categories.Find(CategoryId);
+            if (dbEntry != null)
+            {
+                context.Categories.Remove(dbEntry);
+                context.SaveChanges();
+            }
+        }
+    }
+}
